4e068fccc408a98c73f2441bfe5e8e50412af513
AnooshaMalik <anooshaduet@gmail.com>
2020-06-25 01:59:32 -0700

    done with all transaction randering

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -4,7 +4,8 @@ var auth = firebase.auth();
 var Name = document.querySelector('.name h3');
 var signOutBtn = document.querySelector('.signOutBtn');
 var transactionFormAll = document.querySelector('.transactionFormAll');
-console.log(transactionFormAll);
+var transactionList = document.querySelector('.transactionList');
+// console.log(transactionFormAll);
 // console.log(Name)
 // console.log(firestore);
 // --------------------------------------------------------------------------------------------
@@ -33,17 +34,33 @@ auth.onAuthStateChanged(async(user) => {
             // var { uid } = user;
             uid = user.uid;
             // console.log(uid);
-
             // var User = user.data();
             // ------------------------------> function will call here so that
             // if user is not log init can not do any sort of work <--------------------------------------
             var userInfo = await fetchUserInfo(uid);
-            console.log(userInfo);
+            // console.log(userInfo);
 
             Name.textContent = userInfo.fullName;
+            // var transArr = [{
+            //     title: "abc",
+            //     cost: "200",
+            //     transactionType: "expense",
+            //     transactionAt: "2020-9-3"
+            // }, {
+            //     title: "xyz",
+            //     cost: "300",
+            //     transactionType: "income",
+            //     transactionAt: "2020-10-3"
+            // }, ];
+            var transactions = await fetchTransactions(uid);
+            console.log(transactions);
+            rendorTransactions(transactions);
+
+            // await fetchTransactions(uid);
 
         } else
             location.assign('./index.html');
+
 
 
     })
@@ -53,32 +70,74 @@ var userSignOut = async() => {
     }
     //-------------------------------------------------------------------------------
 var transactionFormSubmission = async(e) => {
-    e.preventDefault();
+        e.preventDefault();
 
-    try {
-        console.log('function starts')
-        var title = document.querySelector('.title').value;
-        var cost = document.querySelector('.cost').value;
-        var select = document.querySelector('.select').value;
-        var transactionAt = document.querySelector('.transactionAt').value;
-        if (title && cost && select && transactionAt) {
-            var transactionObj = {
-                title,
-                cost,
-                select,
-                transactionAt: new Date(transactionAt),
-                transactionBy: uid
+        try {
+            console.log('function starts')
+            var title = document.querySelector('.title').value;
+            var cost = document.querySelector('.cost').value;
+            var select = document.querySelector('.select').value;
+            var transactionAt = document.querySelector('.transactionAt').value;
+            if (title && cost && select && transactionAt) {
+                var transactionObj = {
+                    title,
+                    cost,
+                    select,
+                    transactionAt: new Date(transactionAt),
+                    transactionBy: uid
+                }
+                console.log(transactionObj);
             }
-            console.log(transactionObj);
-        }
-        await firestore.collection("transactions").add(transactionObj);
+            await firestore.collection("transactions").add(transactionObj);
+            var transactions = await fetchTransactions(uid);
+            // console.log(transactions);
+            rendorTransactions(transactions);
 
-    } catch (error) {
-        console.log(error.message);
+        } catch (error) {
+            console.log(error.message);
+        }
+
     }
+    //------------------------- Fetch Tranctions----------------------------
+
+var fetchTransactions = async(uid) => {
+
+        var transactions = [];
+        var query = await firestore.collection("transactions").where("transactionBy", "==", uid).get();
+        // console.log(query);
+        query.forEach((doc) => {
+            transactions.push({...doc.data(), transactionId: doc.id });
+        })
+        return transactions;
+    }
+    //------------------------- Rendor Tranctions----------------------------
+var rendorTransactions = (transactionArr) => {
+    transactionList.innerHTML = " ";
+    transactionArr.forEach((transactions, index) => {
+        var { title, cost, transactionAt, transactionId } = transactions;
+        transactionList.insertAdjacentHTML('beforeend', `   <div class="transactionListItem">
+        <div class="rendorIndex flex itemPadding">
+            <h3>${++index}</h3>
+        </div>
+        <div class="rendorTitle itemPadding ">
+            ${transactions.title}
+        </div>
+        <div class="rendorCost flex itemPadding">
+        ${transactions.cost}
+        </div>
+        <div class="rendorDate flex itemPadding">
+        ${transactions.transactionAt.toDate().toISOString().split('T')[0]}
+        </div>
+        <div class="rendorDate flex itemPadding">
+        <a href="./transaction.html#${transactionId}"> <button type="button">View</button></a>
+        </div>
+
+    </div>`)
+
+    });
+
 
 }
-
 
 
 //-------------------------------------------------------------------------------
@@ -87,3 +146,5 @@ var transactionFormSubmission = async(e) => {
 
 signOutBtn.addEventListener('click', userSignOut);
 transactionFormAll.addEventListener('submit', (e) => transactionFormSubmission(e));
+// transactionFormAll.addEventListener('submit', (e) => transactionFormSubmission(e));
+// transactionFormAll.addEventListener('submit', (e) => transactionFormSubmission(e));
